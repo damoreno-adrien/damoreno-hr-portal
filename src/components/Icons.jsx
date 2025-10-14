6e29dcd73522d6a8ec1197e32b4d55e20235dece
Adrien <damoreno.adrien@gmail.com>
2025-10-14 22:17:03 +0700

    feat(icons): adding Info icon

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -31,3 +31,19 @@ export const XCircleIcon = ({ className }) => (
         <line x1="9" y1="9" x2="15" y2="15"></line>
     </svg>
 );
+export const InfoIcon = ({ className }) => (
+    <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        className={className} 
+        fill="none" 
+        viewBox="0 0 24 24" 
+        stroke="currentColor" 
+        strokeWidth={2}
+    >
+        <path 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" 
+        />
+    </svg>
+);
