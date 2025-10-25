0fa79432b45988bdf1598f3765771d827f05c298
Adrien <damoreno.adrien@gmail.com>
2025-10-25 19:30:13 +0700

    feat: cleaning try catch on

--- functions/index.js
+++ functions/index.js
@@ -7,20 +7,7 @@ const { setStaffAuthStatusHandler } = require('./src/auth/setStaffAuthStatus');
 const { setStaffPasswordHandler } = require('./src/auth/setStaffPassword');
 const { autoCheckoutHandler } = require('./src/attendance/autoCheckout');
 const { calculateAdvanceEligibilityHandler } = require('./src/financials/calculateAdvanceEligibility');
-
-// *** ADD LOGGING AROUND THIS IMPORT ***
-console.log("index.js: Attempting to require calculateLivePayEstimateHandler...");
-let calculateLivePayEstimateHandler;
-try {
-    calculateLivePayEstimateHandler = require('./src/financials/calculateLivePayEstimate').calculateLivePayEstimateHandler;
-    console.log("index.js: Successfully required calculateLivePayEstimateHandler.");
-} catch (error) {
-    console.error("index.js: FAILED to require calculateLivePayEstimateHandler:", error);
-    // Optionally re-throw or handle if needed, but logging might be enough for now
-    // throw error; // Uncomment this if deployment should fail loudly on require error
-}
-// *** END LOGGING BLOCK ***
-
+const { calculateLivePayEstimateHandler } = require('./src/financials/calculateLivePayEstimate');
 const { calculateBonusHandler } = require('./src/payroll/calculateBonus');
 const { deletePayrollRunHandler } = require('./src/payroll/deletePayrollRun');
 const { finalizeAndStorePayslipsHandler } = require('./src/payroll/finalizeAndStorePayslips');
@@ -35,7 +22,6 @@ exports.setStaffAuthStatus = setStaffAuthStatusHandler;
 exports.setStaffPassword = setStaffPasswordHandler;
 exports.autoCheckout = autoCheckoutHandler;
 exports.calculateAdvanceEligibility = calculateAdvanceEligibilityHandler;
-// Ensure the handler is exported even if the require failed (it will likely be undefined and cause errors later, but allows deployment)
 exports.calculateLivePayEstimate = calculateLivePayEstimateHandler;
 exports.calculateBonus = calculateBonusHandler;
 exports.deletePayrollRun = deletePayrollRunHandler;
